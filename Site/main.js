f79aec010608e5920bbe272175ab7e4c0ed44860
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-12-13 13:27:23 -0700

    Pushing old changes from like a month ago

--- Site/main.js
+++ Site/main.js
@@ -1,11 +1,19 @@
+// import { modalBuilder } from "/Site/modalBuilder.js";
+
 const plusButton = document.querySelector("#newItem");
-const newItemModal = document.querySelector("#newItemModal") //Use this for the entire Modal.
-const closeButton = document.querySelector(".closeModal");
-const cancelButton = document.querySelector(".cancelButton");
-const createItem = document.querySelector(".createItem");
-const itemTypeDropdown = document.querySelector("#itemTypeMenu");
-const itemTypeButton = document.querySelector(".dropdownButton")
-console.log(closeButton);
+    const newItemModal = document.querySelector("#newItemModal") //Use this for the entire Modal.
+    const closeButton = document.querySelector(".closeModal");
+    const cancelButton = document.querySelector(".cancelButton");
+    const createItem = document.querySelector(".createItem");
+    const itemTypeDropdown = document.querySelector("#itemTypeMenu");
+    const itemTypeButton = document.querySelector(".dropdownButton")
+    const addField = document.querySelector(".add-field");
+    const modalBody = document.querySelector(".modal-body");
+function queryDocument(){
+    console.log('click');
+};
+queryDocument();
+
 let itemProperty = ["name", "parentItem", "childItem", "price", "description", "purchaseDate"]
 let itemType = ["electronic", "collectable", "plush"];
 function buildItemTypes(type){
@@ -24,12 +32,20 @@ function clearAddItemModal(){
     newItemModal.innerHTML = ``
 }
 
+//This is the blue new item button
 plusButton.addEventListener('click', e => {
     e.preventDefault();
     console.log("Click");
     newItemModal.classList.add("shown");
 });
-
+//for adding an aditional field to the item
+addField.addEventListener('click', e => {
+    e.preventDefault();
+    modalBody.innerHTML += `<div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Custom Data">
+                            </div>`
+    queryDocument();
+})
 closeButton.addEventListener('click', e => {
     e.preventDefault();
     newItemModal.classList.remove("shown");
@@ -56,3 +72,13 @@ createItem.addEventListener('click', e => {
     //send to database
     //call method to clear Add New Item Card.
 });
+
+//Function to pull data from database
+
+//function to parse JSON data into a 2d array
+function itemDataArray(){
+
+}
+
+//function to call the modalBuilder function and update the modal 
+
